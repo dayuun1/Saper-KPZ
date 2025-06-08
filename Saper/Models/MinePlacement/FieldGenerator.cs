6e0b6ed12eba0a24ff2079ead38b707cb96b4ef0
BohdanBychak <ipz232_bbm@student.ztu.edu.ua>
2025-06-08 15:47:24 +0300

    Створено допоміжні методи для покращення читабельності

--- Saper/Models/MinePlacement/FieldGenerator.cs
+++ Saper/Models/MinePlacement/FieldGenerator.cs
@@ -12,36 +12,35 @@ namespace Saper.Models.MinePlacement
         public Cell[,] GenerateMinefield(int rows, int columns, int mineCount, out int cellsToOpen)
         {
             Cell[,] cells = new Cell[rows, columns];
-            bool[] mineArr = _minePlacementStrategy.GenerateMines(rows, columns, mineCount);
+            bool[] minesArray = _minePlacementStrategy.GenerateMines(rows, columns, mineCount);
             cellsToOpen = rows * columns - mineCount;
 
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < columns; j++)
                 {
-                    bool isMine = mineArr[i * columns + j];
-                    CellType cellType;
-
-                    if (isMine)
-                    {
-                        cellType = CellType.Mine;
-                    }
-                    else
-                    {
-                        int mineNear = CountNearbyMines(mineArr, rows, columns, i, j);
-                        cellType = (CellType)mineNear;
-                    }
-
-                    cells[i, j] = new Cell(isMine, cellType);
+                    cells[i, j] = CreateCell(i, j, minesArray, rows, columns);
                 }
             }
 
             return cells;
         }
 
+        private Cell CreateCell(int row, int column, bool[] minesArray, int rows, int columns)
+        {
+			bool isMine = minesArray[GetIndex(row, column, columns)];
+			CellType cellType = isMine 
+                ? CellType.Mine 
+                : (CellType) CountNearbyMines(minesArray, rows, columns, row, column);
+            return new Cell(isMine, cellType);
+
+		}
+
+		private int GetIndex(int row, int column, int columns) => row * columns + column;
+
         private int CountNearbyMines(bool[] minesArr, int rows, int columns, int i, int j)
         {
-            int mineNear = 0;
+            int nearbyMinesCount = 0;
             for (int x = -1; x <= 1; x++)
             {
                 for (int y = -1; y <= 1; y++)
@@ -52,13 +51,13 @@ namespace Saper.Models.MinePlacement
                     if ((x != 0 || y != 0) &&
                         ni >= 0 && nj >= 0 &&
                         ni < rows && nj < columns &&
-                        minesArr[ni * columns + nj])
+                        minesArr[GetIndex(ni, nj, columns)])
                     {
-                        mineNear++;
+						nearbyMinesCount++;
                     }
                 }
             }
-            return mineNear;
+            return nearbyMinesCount;
         }
     }
 }
